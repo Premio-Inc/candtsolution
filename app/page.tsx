16db9f60f011c830259422f5ff2a0da6117d1545
Charles Chau <chausieu@gmail.com>
2025-12-23 14:55:18 -0800

    draft 1

--- app/page.tsx
+++ app/page.tsx
@@ -6,28 +6,18 @@ import {
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[url(https://cdn.shopify.com/s/files/1/0028/7509/7153/files/Smart_city_and_abstract_dot_point_connect_with_gradient_line_stock_photo_1920x.jpg?v=1596750954)] bg-cover bg-center bg-no-repeat container-fluid p-5 flex align-center justify-center">
-      <Container maxWidth="md" className="my-5 py-5 bg-slate-100 mx-auto row justify-center align-center rounded min-h-[80vh]">
+    <div className=" bg-[url(https://cdn.shopify.com/s/files/1/0028/7509/7153/files/Smart_city_and_abstract_dot_point_connect_with_gradient_line_stock_photo_1920x.jpg?v=1596750954)] bg-cover bg-center bg-no-repeat container-fluid p-5 flex align-center justify-center">
+      <Container maxWidth="md" className="my-5 py-5 bg-slate-100 mx-auto row justify-center align-center rounded ">
         <div className="col-lg-8 space-y-5">
           <div className="w-full rounded overflow-hidden">
-            <Image src="https://picsum.photos/900/300" alt="C&T is becoming Premio Inc." width={900} height={300} unoptimized />
+            <Image src="https://cdn.shopify.com/s/files/1/0028/7509/7153/files/C_T_Solution_is_now_Premio_Inc.png?v=1766518766" alt="C&T is becoming Premio Inc." width={900} height={300} unoptimized />
           </div>
 
-          <h1 className="capitalize text-center">C&T is becoming Part of Premio Inc.</h1>
+          <h1 className="capitalize text-center">Thank you for being part of our journey! </h1>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio at itaque optio. Quaerat totam delectus quis aliquam sunt aperiam, facilis ab laudantium libero natus ducimus id dolore laborum beatae fugit!
-
+            We are now part of Premio&apos;s global brand. Our unified global structure allows us to innovate faster, scale with confidence, and support the growing demands of edge AI and industrial automation worldwide.
           </p>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, blanditiis aliquam? Animi nam deserunt obcaecati ex maxime at soluta voluptatem, harum eaque perspiciatis error aperiam culpa illum maiores. Laboriosam, aut?
-          </p>
-
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere quisquam neque quo iure tempore culpa autem consequuntur ea facilis corporis libero sapiente assumenda, nobis et repellendus commodi. Possimus, eveniet labore!
-          </p>
-
 
           <div className="text-center">
             <Button
@@ -39,9 +29,66 @@ export default function Home() {
               component="a"
               variant="contained"
             >
-              Go to Premio Inc.
+              Visit Premio Inc.
             </Button>
           </div>
+          <hr className="border-zinc-300" />
+          <div className="grid align-start justify-center grid-cols-1 lg:grid-cols-2 gap-5">
+            <div className="col lg:border-r border-zinc-300">
+              <div className="my-3 flex justify-center">
+                <Image src="https://cdn.shopify.com/s/files/1/0028/7509/7153/files/C_T_Solution_transition_to_Premio_Press_Release.png?v=1766518766" width={300} height={300} alt="" unoptimized />
+              </div>
+              <p>
+                As of January 1, 2026, Premio Inc. and C&T Solution Inc. unify under one global brand to strengthen engineering, manufacturing, and support worldwide.
+              </p>
+
+              <div className="my-3 text-center">
+                <Button
+                  sx={{
+                    backgroundColor: "#7d2248",
+                    color: "var(--color-white)"
+                  }}
+                  href="https://premioinc.com/blogs/press-kits/premio-inc-united-states-and-c-t-solution-inc-taiwan-consolidate-under-one-brand-to-advance-edge-ai-leadership-in-2026-and-beyond"
+                  component="a"
+                  variant="contained"
+                >
+                  Read Press Release
+                </Button>
+              </div>
+
+
+            </div>
+            <div className="col">
+              <div className="my-3 flex justify-center">
+                <Image src="https://cdn.shopify.com/s/files/1/0028/7509/7153/files/Industrial_Computing_Solutions_300x.png?v=1766518766" width={300} height={300} alt="" unoptimized />
+              </div>
+
+              <ul className="list-disc ms-5">
+                <li>X86 Super-Rugged Edge Computers  </li>
+                <li>X86 Semi-Rugged Industrial Computers </li>
+                <li>NVIDIA Jetson Edge AI Computers  </li>
+                <li>Industrial Edge AI Servers  </li>
+                <li>Industrial HMIs and Panel PCs  </li>
+                <li>Industrial Motherboards and SBCs  </li>
+              </ul>
+
+              <div className="my-3 text-center">
+                <Button
+                  sx={{
+                    backgroundColor: "#7d2248",
+                    color: "var(--color-white)"
+                  }}
+                  href="https://premioinc.com/products"
+                  component="a"
+                  variant="contained"
+                >
+                  Explore Products
+                </Button>
+              </div>
+            </div>
+          </div>
+
+
         </div>
       </Container>
     </div>
---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+import colors from "tailwindcss/colors";
+
+const config = {
+  theme: {
+    extend: {
+      borderColor: {
+        DEFAULT: colors.slate[300],
+      },
+    },
+  },
+};
+
+export default config;
